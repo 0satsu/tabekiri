722d0e1bf853c154424bb51d95eed2c991421f30
Osatsu-cat <moluca5460@gmail.com>
2019-06-28 12:48:00 +0900

    add user table $ default callback

---
+++ app/controllers/linebot_controller.rb
@@ -0,0 +1,84 @@
+class LinebotController < ApplicationController
+  require 'line/bot' # gem 'line-bot-api'
+  require 'open-uri' #note
+  require 'kconv' #note
+  require 'rexml/document' #note
+
+  # callbackアクションのCSRFトークン認証を無効 #note
+  protect_from_forgery :except => [:callback]
+
+  post '/callback' do
+    body = request.body.read
+
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      error 400 do 'Bad Request' end
+    end
+
+    events = client.parse_events_from(body)
+    events.each do |event|
+      case event
+      # メッセージが送信された場合の対応（機能①）
+      when Line::Bot::Event::Message
+        case event.type
+        # ユーザーからテキスト形式のメッセージが送られて来た場合
+        when Line::Bot::Event::MessageType::Text
+          # event.message['text']：ユーザーから送られたメッセージ
+          input = event.message['text']
+
+          case input
+            # 「明日」or「あした」というワードが含まれる場合
+          when /.*(明日|あした).*/
+            push = ""
+          when /.*(明後日|あさって).*/
+            push = ""
+          when /.*(かわいい|可愛い|カワイイ|きれい|綺麗|キレイ|素敵|ステキ|すてき|面白い|おもしろい|ありがと|すごい|スゴイ|スゴい|好き|頑張|がんば|ガンバ).*/
+            push =
+              "ありがとう！！！\n優しい言葉をかけてくれるあなたはとても素敵です(^^)"
+          when /.*(こんにちは|こんばんは|初めまして|はじめまして|おはよう).*/
+            push =
+              "こんにちは。\n声をかけてくれてありがとう\n今日があなたにとっていい日になりますように(^^)"
+          else
+
+          end
+          # テキスト以外（画像等）のメッセージが送られた場合
+        else
+          push = "テキスト以外はわからないよ〜(；；)"
+        end
+
+          #送信するメッセージを定義
+          message = {
+            type: 'text',
+            text: push
+          }
+          client.reply_message(event['replyToken'], message)
+      # LINEお友達追された場合（機能②）
+      when Line::Bot::Event::Follow
+        # 登録したユーザーのidをユーザーテーブルに格納
+        line_id = event['source']['userId']
+        User.create(line_id: line_id)
+        # LINEお友達解除された場合（機能③）
+      when Line::Bot::Event::Unfollow
+        # お友達解除したユーザーのデータをユーザーテーブルから削除
+        line_id = event['source']['userId']
+        User.find_by(line_id: line_id).destroy
+      end
+
+
+    end
+
+    # Don't forget to return a successful response
+    "OK"
+  end
+
+
+  private
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_id = ENV["LINE_CHANNEL_ID"]
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #メッセージが来た時、友達追加・解除がされた時にcallbackアクションを呼び出す
+  post '/callback' => 'linebot#callback'
 end

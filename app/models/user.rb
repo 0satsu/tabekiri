c6d5dc0dc0db309b84bdf01e143c736fbff7ae9c
Osatsu-cat <moluca5460@gmail.com>
2019-09-04 20:32:40 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_many :reminds, dependent: :delete_all
   
-  validates :last_message, length: {maximun: 100}, allow_nil: true
+  validates :last_message, length: {maximum: 100}, allow_nil: true
 end
